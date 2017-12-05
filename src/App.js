ced73767b011a2ca977530bfc0d4035921700692
JulianMayorga <mayorga.julian@gmail.com>
2017-12-05 19:42:08 -0300

    Show train

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { View, StyleSheet } from "react-native";
 const skyblue = "#AFD4FF";
 const brown = "#E7A871";
 const black = "#4A4A4A";
+const red = "#D0011B";
+
+const trainHeight = 100;
+const railwayHeight = 5;
 
 const styles = StyleSheet.create({
   container: {
@@ -22,8 +26,26 @@ const styles = StyleSheet.create({
     height: "20%",
     borderTopStyle: "solid",
     borderTopColor: black,
-    borderTopWidth: 5
+    borderTopWidth: railwayHeight
   },
+  train: {
+    position: "absolute",
+    width: "50%",
+    height: trainHeight,
+    backgroundColor: red,
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    top: -trainHeight / 2 - railwayHeight,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    maxWidth: 300
+  },
+  window: {
+    height: "50%",
+    width: "10%",
+    backgroundColor: black
+  }
 });
 
 class App extends Component {
@@ -31,7 +53,15 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.sky} />
-        <View style={styles.land} />
+        <View style={styles.land}>
+          <View style={styles.train}>
+            <View style={styles.window} />
+            <View style={styles.window} />
+            <View style={styles.window} />
+            <View style={styles.window} />
+            <View style={styles.window} />
+          </View>
+        </View>
       </View>
     );
   }
